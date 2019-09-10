2f56e725f23f36e94c3633c0fbaa9ee0717dd2ad
Karan <karangupta199920@gmail.com>
2019-09-10 13:28:30 +0530

    minor changes

--- script.js
+++ script.js
@@ -3,13 +3,31 @@ import './style.css';
 
 let circle = [];
 if (window.innerHeight < window.innerWidth) {
-  circle[0] = { x: 'calc(50vw - 4.8em)', y: 0 };
-  circle[1] = { x: 'calc(50vw - 4.8em)', y: '110%' };
-  circle[2] = { x: 'calc(50vw - 4.8em)', y: '220%' };
+  circle[0] = {
+    x: 'calc(50vw - 4.8em)',
+    y: 0
+  };
+  circle[1] = {
+    x: 'calc(50vw - 4.8em)',
+    y: '110%'
+  };
+  circle[2] = {
+    x: 'calc(50vw - 4.8em)',
+    y: '220%'
+  };
 } else {
-  circle[0] = { x: '-110%', y: '20vh' };
-  circle[1] = { x: 0, y: '20vh' };
-  circle[2] = { x: '110%', y: '20vh' };
+  circle[0] = {
+    x: '-110%',
+    y: '20vh'
+  };
+  circle[1] = {
+    x: 0,
+    y: '20vh'
+  };
+  circle[2] = {
+    x: '110%',
+    y: '20vh'
+  };
 }
 
 let t1 = anime.timeline({
@@ -18,13 +36,25 @@ let t1 = anime.timeline({
 });
 
 t1.add({
-  targets: '#circle1',
-  keyframes: [
-    { translateX: 0, translateY: 0, scale: [0, 9], duration: 500 },
-    { scale: 1, duration: 500, delay: 700 },
-    { translateX: circle[0].x, translateY: circle[0].y, duration: 250 }
-  ]
-})
+    targets: '#circle1',
+    keyframes: [{
+        translateX: 0,
+        translateY: 0,
+        scale: [0, 9],
+        duration: 500
+      },
+      {
+        scale: 1,
+        duration: 500,
+        delay: 700
+      },
+      {
+        translateX: circle[0].x,
+        translateY: circle[0].y,
+        duration: 250
+      }
+    ]
+  })
   .add({
     targets: '#camera-svg',
     opacity: 1,
@@ -33,10 +63,22 @@ t1.add({
   }) //circle2
   .add({
     targets: '#circle2',
-    keyframes: [
-      { translateX: 0, translateY: 0, scale: [0, 9], duration: 500 },
-      { scale: 1, duration: 500, delay: 700 },
-      { translateX: circle[1].x, translateY: circle[1].y, duration: 250 }
+    keyframes: [{
+        translateX: 0,
+        translateY: 0,
+        scale: [0, 9],
+        duration: 500
+      },
+      {
+        scale: 1,
+        duration: 500,
+        delay: 700
+      },
+      {
+        translateX: circle[1].x,
+        translateY: circle[1].y,
+        duration: 250
+      }
     ]
   })
   .add({
@@ -47,10 +89,22 @@ t1.add({
   }) //circle3
   .add({
     targets: '#circle3',
-    keyframes: [
-      { translateX: 0, translateY: 0, scale: [0, 9], duration: 500 },
-      { scale: 1, duration: 500, delay: 700 },
-      { translateX: circle[2].x, translateY: circle[2].y, duration: 250 }
+    keyframes: [{
+        translateX: 0,
+        translateY: 0,
+        scale: [0, 9],
+        duration: 500
+      },
+      {
+        scale: 1,
+        duration: 500,
+        delay: 700
+      },
+      {
+        translateX: circle[2].x,
+        translateY: circle[2].y,
+        duration: 250
+      }
     ]
   })
   .add({
@@ -104,20 +158,41 @@ for (let i = 0; i < textWrapper.length; i++)
     "<span class='letter'>$&</span>"
   );
 
-const targets = [
-  { id: '#circle1', icon: '#camera-svg', div: '#photography', svgclass: ' .p' },
-  { id: '#circle2', icon: '#code-svg', div: '#code', svgclass: ' .q' },
-  { id: '#circle3', icon: '#design-svg', div: '#design', svgclass: ' .a' }
+const targets = [{
+    id: '#circle1',
+    icon: '#camera-svg',
+    div: '#photography',
+    svgclass: ' .p'
+  },
+  {
+    id: '#circle2',
+    icon: '#code-svg',
+    div: '#code',
+    svgclass: ' .q'
+  },
+  {
+    id: '#circle3',
+    icon: '#design-svg',
+    div: '#design',
+    svgclass: ' .a'
+  }
 ];
 
 let target;
-
+let open_flag = 0;
 const open = index => {
+  open_flag = 1;
   target = targets[index];
   anime({
     targets: target.id,
-    translateX: { value: circle[index].x, duration: 0 },
-    translateY: { value: circle[index].y, duration: 0 },
+    translateX: {
+      value: circle[index].x,
+      duration: 0
+    },
+    translateY: {
+      value: circle[index].y,
+      duration: 0
+    },
     scale: 50,
     duration: 250,
     easing: 'easeInCirc'
@@ -140,7 +215,9 @@ const open = index => {
     translateX: [40, 0],
     opacity: [0, 1],
     duration: 1200,
-    delay: anime.stagger(30, { start: 500 })
+    delay: anime.stagger(30, {
+      start: 500
+    })
   });
 
   if (window.innerWidth < 800) {
@@ -155,6 +232,7 @@ const open = index => {
 };
 
 const close = () => {
+  open_flag = 0;
   if (window.innerWidth < 800) {
     anime({
       targets: target.icon,
@@ -166,8 +244,14 @@ const close = () => {
   }
   anime({
     targets: target.id,
-    translateX: { value: circle[target.id.charAt(7) - 1].x, duration: 0 },
-    translateY: { value: circle[target.id.charAt(7) - 1].y, duration: 0 },
+    translateX: {
+      value: circle[target.id.charAt(7) - 1].x,
+      duration: 0
+    },
+    translateY: {
+      value: circle[target.id.charAt(7) - 1].y,
+      duration: 0
+    },
     scale: [50, 1],
     duration: 250,
     easing: 'easeOutCirc'
@@ -184,3 +268,9 @@ icons.forEach(
 );
 
 document.querySelector('.close').onclick = close;
+
+window.addEventListener('keyup', function (e) {
+  if (e.keyCode == 27 && open_flag) {
+    close();
+  }
+});
